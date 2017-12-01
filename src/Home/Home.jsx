0a516ca74d9d7a378a5c460def7cf4cc9e1f3229
Daniel Kocjan <dkocjan123@gmail.com>
2017-12-01 12:31:25 +0100

    Implement styled components in Home component

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    background: lavenderblush;
+`,
+      Heading = styled.h1`
+padding: 15px 0 15px 0px;
+text-align: center;
+`
 
 const Home = () => (
-  <div className="landing">
-    <h1>React Boilerplate</h1>
-  </div>
+  <Wrapper>
+    <Heading>React Boilerplate</Heading>
+  </Wrapper>
 );
 
 export default Home;
